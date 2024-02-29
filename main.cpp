4318eac3ca97ecb24bcc1b65b5ed6222c2c81d41
김해든 <haidn1994@gmail.com>
2024-02-29 12:49:41 +0900

    초기 커밋.

---
+++ main.cpp
@@ -0,0 +1,11 @@
+﻿#include "pch.h"
+#include "std_lib_facilities.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!\n"; // "Hello, World!" 출력
+    keep_window_open();
+    return 0;
+}
